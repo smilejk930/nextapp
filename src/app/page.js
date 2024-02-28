212a39aa9f72dde4514cc89de066d94be0214e34
smilejk <smilejk930@gmail.com>
2024-02-29 04:37:58 +0900

    오타 수정

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <>
-      <h2>Wellcome</h2>
+      <h2>Welcome</h2>
       Hello WEB!
       <br />
       <img src="/apple.png" width={100} height={100} />
