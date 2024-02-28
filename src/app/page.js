a6f5c79579814871df5477418a549df62d0a5ffb
smilejk <smilejk930@gmail.com>
2024-02-29 03:53:16 +0900

    Feat: 이미지 및 css 추가

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,12 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return <>Hello, Nextjs!</>
+  return (
+    <>
+      <h2>Wellcome</h2>
+      Hello WEB!
+      <br />
+      <img src="/apple.png" width={100} height={100} />
+    </>
+  )
 }
