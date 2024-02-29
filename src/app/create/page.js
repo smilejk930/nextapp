747b820d7798b1a478422b9b33c1db07be9667ee
kjk <jinkwang930@ictway.co.kr>
2024-02-29 17:47:46 +0900

    Feat: 글쓰기를 위한 useRouter().push() 사용

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,3 +1,41 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
 export default function Create() {
-  return <>Create!</>
+  const router = useRouter() // redirection
+  return (
+    <form
+      // Client Component
+      onSubmit={(e) => {
+        e.preventDefault() // submit 이벤트 전달 막기
+        const title = e.target.title.value
+        const body = e.target.body.value
+        const options = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ title, body }),
+        }
+        fetch(`http://localhost:9999/topics`, options)
+          .then((res) => res.json())
+          .then((result) => {
+            console.log(result)
+            const lastid = result.id // lastid를 redirection 시키기 위해 useRouter 사용(import 주소 주의! navigation으로 해야 함)
+            router.push(`/read/${lastid}`)
+          })
+      }}
+    >
+      <p>
+        <input type="text" name="title" placeholder="Title" />
+      </p>
+      <p>
+        <textarea name="body" placeholder="Body" />
+      </p>
+      <p>
+        <input type="submit" value="Create" />
+      </p>
+    </form>
+  )
 }
